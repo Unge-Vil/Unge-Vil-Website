4672c4f1cd900f0c461a55cde2742e3197ddf4dd
naleyjanhelge <naleyjanhelge@gmail.com>
2025-08-21 14:40:05 +0200

    Add team grid block

---
+++ plugins/uv-people/blocks/team-grid/index.js
@@ -0,0 +1,43 @@
+( function( wp ) {
+    const { createElement } = wp.element;
+    const { registerBlockType } = wp.blocks;
+    const { __ } = wp.i18n;
+    const { InspectorControls } = wp.blockEditor;
+    const { PanelBody, SelectControl, RangeControl } = wp.components;
+    const { useSelect } = wp.data;
+
+    registerBlockType( 'uv/team-grid', {
+        edit: function( props ) {
+            const { attributes: { location, columns }, setAttributes } = props;
+            const terms = useSelect( function( select ) {
+                return select( 'core' ).getEntityRecords( 'taxonomy', 'uv_location', { per_page: -1 } );
+            }, [] );
+            const options = terms ? terms.map( function( t ) {
+                return { label: t.name, value: t.slug };
+            } ) : [];
+            return [
+                createElement( InspectorControls, {},
+                    createElement( PanelBody, { title: __( 'Settings', 'uv-people' ), initialOpen: true },
+                        createElement( SelectControl, {
+                            label: __( 'Location', 'uv-people' ),
+                            value: location,
+                            options: [ { label: __( 'Select', 'uv-people' ), value: '' } ].concat( options ),
+                            onChange: function( value ) { setAttributes( { location: value } ); }
+                        } ),
+                        createElement( RangeControl, {
+                            label: __( 'Columns', 'uv-people' ),
+                            min: 1,
+                            max: 6,
+                            value: columns,
+                            onChange: function( value ) { setAttributes( { columns: value } ); }
+                        } )
+                    )
+                ),
+                createElement( 'p', {}, __( 'Team Grid', 'uv-people' ) )
+            ];
+        },
+        save: function() {
+            return null;
+        }
+    } );
+} )( window.wp );
