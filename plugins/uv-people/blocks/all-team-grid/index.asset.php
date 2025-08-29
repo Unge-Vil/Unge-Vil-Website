8c84bd3d4e69c8a590ddb282ca26db3c77f5c302
naleyjanhelge <naleyjanhelge@gmail.com>
2025-08-29 18:31:49 +0200

    Use FormTokenField for location multi-select

--- plugins/uv-people/blocks/all-team-grid/index.asset.php
+++ plugins/uv-people/blocks/all-team-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components', 'wp-i18n', 'wp-server-side-render', 'wp-data'), 'version' => '6fd0b588a08a7ffa88a8');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components', 'wp-i18n', 'wp-server-side-render', 'wp-data'), 'version' => 'bb41e67aa85f6e3af4ec');
