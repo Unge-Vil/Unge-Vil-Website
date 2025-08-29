796c20e3b79aa55d2b058515027205748a1ecfd8
naleyjanhelge <naleyjanhelge@gmail.com>
2025-08-29 18:25:42 +0200

    chore: add wp-data dependency to people blocks

--- plugins/uv-people/blocks/team-grid/index.asset.php
+++ plugins/uv-people/blocks/team-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components', 'wp-api-fetch', 'wp-i18n', 'wp-server-side-render'), 'version' => '1.0.0');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components', 'wp-i18n', 'wp-server-side-render', 'wp-data'), 'version' => 'cf1cd1355b7900ff04f8');
