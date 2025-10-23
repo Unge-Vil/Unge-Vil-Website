80ec5466e8394f5925985bc47f7cb8be5adf247b
naleyjanhelge <naleyjanhelge@gmail.com>
2025-10-23 15:52:14 +0200

    Improve experience partner display and admin selectors

--- plugins/uv-core/assets/admin.js
+++ plugins/uv-core/assets/admin.js
@@ -34,4 +34,21 @@ jQuery(function($){
 
         $select.select2(options);
     });
+
+    $(document).on('click', '.uv-clear-selection', function(e){
+        e.preventDefault();
+
+        var target = $(this).data('target');
+        var $select = target ? $(target) : $(this).closest('.uv-meta-actions').prev('select');
+
+        if (!$select || !$select.length) {
+            return;
+        }
+
+        $select.find('option:selected').prop('selected', false);
+        $select.val(null).trigger('change');
+        if ($.fn.select2 && $select.data('select2')) {
+            $select.trigger('change.select2');
+        }
+    });
 });
