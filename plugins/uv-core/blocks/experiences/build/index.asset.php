cad369240c5315702e5d4b65c3c763b1c128a271
naleyjanhelge <naleyjanhelge@gmail.com>
2025-12-02 16:40:30 +0100

    Add load more support to experiences block

--- plugins/uv-core/blocks/experiences/build/index.asset.php
+++ plugins/uv-core/blocks/experiences/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '4931bc7ed1853f895a54');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '030c80cfa3b9b80e51de');
